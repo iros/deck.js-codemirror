712bcc2aa613b806ed675d580fd5f82950ae818f
Irene Ros <irene@bocoup.com>
2012-12-21 15:54:09 -0500

    Upgrading CodeMirror to 3.0

--- mode/lua/lua.js
+++ mode/lua/lua.js
@@ -50,7 +50,7 @@ CodeMirror.defineMode("lua", function(config, parserConfig) {
 			 "true","function", "end", "if", "then", "else", "do", 
 			 "while", "repeat", "until", "for", "in", "local" ]);
 
-  var indentTokens = wordRE(["function", "if","repeat","for","while", "\\(", "{"]);
+  var indentTokens = wordRE(["function", "if","repeat","do", "\\(", "{"]);
   var dedentTokens = wordRE(["end", "until", "\\)", "}"]);
   var dedentPartial = prefixRE(["end", "until", "\\)", "}", "else", "elseif"]);
 
@@ -64,7 +64,7 @@ CodeMirror.defineMode("lua", function(config, parserConfig) {
   function normal(stream, state) {
     var ch = stream.next();
     if (ch == "-" && stream.eat("-")) {
-      if (stream.eat("["))
+      if (stream.eat("[") && stream.eat("["))
         return (state.cur = bracketed(readBracket(stream), "comment"))(stream, state);
       stream.skipToEnd();
       return "comment";
@@ -123,8 +123,10 @@ CodeMirror.defineMode("lua", function(config, parserConfig) {
         else if (builtins.test(word)) style = "builtin";
 	else if (specials.test(word)) style = "variable-2";
       }
-      if (indentTokens.test(word)) ++state.indentDepth;
-      else if (dedentTokens.test(word)) --state.indentDepth;
+      if ((style != "comment") && (style != "string")){
+        if (indentTokens.test(word)) ++state.indentDepth;
+        else if (dedentTokens.test(word)) --state.indentDepth;
+      }
       return style;
     },
 
